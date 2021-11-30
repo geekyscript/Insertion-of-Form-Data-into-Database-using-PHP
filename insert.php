c29a3ad92a2f67ca5d984490f31683421a0d0cd9
Geeky Script <85415542+geekyscript@users.noreply.github.com>
2021-11-30 10:19:24 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 <?php
-		$conn = mysqli_connect("64.227.180.62", "root", "uejdfaypkc", "uejdfaypkc");
+		$conn = mysqli_connect("uejdfaypkc@localhost", "root", "uejdfaypkc", "uejdfaypkc");
 		
 		// Checking the connection
 		if($conn === false){
